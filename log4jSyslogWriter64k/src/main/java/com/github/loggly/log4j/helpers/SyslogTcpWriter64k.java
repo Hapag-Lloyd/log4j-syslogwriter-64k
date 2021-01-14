79b462e41720d1fd158cc22ed5b5151f7b2c34fe
goschla <goschla@hlag.com>
2021-01-14 09:53:50 +0100

    ITGWRP-1930 Changed behavior to remarks from code review.

--- log4jSyslogWriter64k/src/main/java/com/github/loggly/log4j/helpers/SyslogTcpWriter64k.java
+++ log4jSyslogWriter64k/src/main/java/com/github/loggly/log4j/helpers/SyslogTcpWriter64k.java
@@ -7,7 +7,6 @@ import java.net.Socket;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /**
  * SyslogWriter64k is a wrapper around the java.net.DatagramSocket class so that
@@ -26,24 +25,17 @@ public class SyslogTcpWriter64k extends SyslogWriter64k {
 
 	private Optional<BufferedWriter> writer = Optional.empty();
 
-	private final int maxTries = 10; // TODO put this in a config?
-
-	private final int sleepTimeInSeconds = 1; // TODO put this in a config?
-
 	private Optional<BufferedWriter> getOptionalWriter() {
 		return writer;
 	}
 
 	private synchronized BufferedWriter getWriter() throws IOException {
 		if (!writer.isPresent()) {
-			connectSocketAndSetWriter();
+			socket = Optional.of(new Socket(getSyslogHost(), getSyslogPort()));
+			writer = Optional
+					.of(new BufferedWriter(new OutputStreamWriter(socket.get().getOutputStream(), getCharset())));
 		}
 		return writer.get();
-	}
-
-	private void connectSocketAndSetWriter() throws IOException {
-		socket = Optional.of(new Socket(getSyslogHost(), getSyslogPort()));
-		writer = Optional.of(new BufferedWriter(new OutputStreamWriter(socket.get().getOutputStream(), getCharset())));
 	}
 
 	@Override
@@ -58,35 +50,8 @@ public class SyslogTcpWriter64k extends SyslogWriter64k {
 		try {
 			getWriter().append(syslogFrame);
 		} catch (final IOException e) {
-			reconnect(syslogFrame);
-		}
-	}
-
-	private void reconnect(final String syslogFrame) throws IOException {
-		int reconnectionTries = 0;
-		while (reconnectionTries < maxTries) {
-			try {
-				reconnectionTries++;
-				connectSocketAndSetWriter();
-				getWriter().append(syslogFrame);
-				return;
-			} catch (final IOException e) {
-				// sleep a while and then try again
-				sleep();
-			}
-		}
-
-		// Reconnecting is currently not possible
-		throw new RuntimeException(
-				String.format("Failed to write log via tcp, tried to reconnect %s times.", reconnectionTries));
-	}
-
-	private void sleep() {
-		try {
-			TimeUnit.SECONDS.sleep(sleepTimeInSeconds);
-		} catch (final InterruptedException e) {
-			// Interrupted.
-			new RuntimeException("Interrupted during waiting on reconnecting to logging socket.", e);
+			close();
+			throw e;
 		}
 	}
 
@@ -111,5 +76,8 @@ public class SyslogTcpWriter64k extends SyslogWriter64k {
 		if (socket.isPresent()) {
 			socket.get().close();
 		}
+
+		writer = Optional.empty();
+		socket = Optional.empty();
 	}
 }
