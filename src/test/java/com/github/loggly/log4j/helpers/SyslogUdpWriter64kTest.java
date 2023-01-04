3e863588092fcf35325903ddc4c39a7197db1888
Lars Knickrehm <mail@lars-sh.de>
2023-01-04 22:23:50 +0100

    Improve unit tests as of Eclipse warnings

--- src/test/java/com/github/loggly/log4j/helpers/SyslogUdpWriter64kTest.java
+++ src/test/java/com/github/loggly/log4j/helpers/SyslogUdpWriter64kTest.java
@@ -1,7 +1,12 @@
 package com.github.loggly.log4j.helpers;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.io.IOException;
 import java.io.Writer;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.Test;
@@ -13,15 +18,34 @@ public class SyslogUdpWriter64kTest {
 
 	@Test
 	public void createWriterWithPort() throws IOException {
-		final Writer writer = new SyslogUdpWriter64k("localhost:5514", StandardCharsets.UTF_8);
-		writer.write("abc");
-		writer.close();
+		// given
+		try (DatagramSocket socket = new DatagramSocket(new InetSocketAddress("localhost", 5514));
+				Writer writer = new SyslogUdpWriter64k("localhost:5514", StandardCharsets.UTF_8)) {
+			// when
+			writer.write("abc");
+
+			// then
+			final byte[] buffer = new byte[3];
+			final DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			assertArrayEquals("abc".getBytes(StandardCharsets.US_ASCII), buffer);
+		}
 	}
 
 	@Test
 	public void createWriterWithoutPort() throws IOException {
-		final Writer writer = new SyslogUdpWriter64k("localhost", StandardCharsets.UTF_8);
-		writer.write("abc");
-		writer.close();
+		// given
+		try (DatagramSocket socket
+				= new DatagramSocket(new InetSocketAddress("localhost", SyslogWriter64k.DEFAULT_SYSLOG_PORT));
+				Writer writer = new SyslogUdpWriter64k("localhost", StandardCharsets.UTF_8)) {
+			// when
+			writer.write("abc");
+
+			// then
+			final byte[] buffer = new byte[3];
+			final DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			assertArrayEquals("abc".getBytes(StandardCharsets.US_ASCII), buffer);
+		}
 	}
 }
