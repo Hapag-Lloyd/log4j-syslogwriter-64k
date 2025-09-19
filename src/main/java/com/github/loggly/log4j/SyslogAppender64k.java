08f7c6cc892016f7bd4900cfdfa725f2166b4664
Lars Knickrehm <mail@lars-sh.de>
2025-09-20 00:45:27 +0200

    Handle PMD warnings

--- src/main/java/com/github/loggly/log4j/SyslogAppender64k.java
+++ src/main/java/com/github/loggly/log4j/SyslogAppender64k.java
@@ -6,9 +6,11 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -131,8 +133,8 @@ public class SyslogAppender64k extends AppenderSkeleton {
 	/**
 	 * Date format used if header = true.
 	 */
-	private static final ThreadLocal<SimpleDateFormat> DATE_FORMAT
-			= ThreadLocal.withInitial(() -> new SimpleDateFormat("MMM dd HH:mm:ss ", Locale.ENGLISH));
+	private static final DateTimeFormatter DATE_FORMAT
+			= DateTimeFormatter.ofPattern("MMM dd HH:mm:ss ", Locale.ENGLISH);
 
 	/**
 	 * Maps integer values to the corresponding syslog facility name.
@@ -633,7 +635,8 @@ public class SyslogAppender64k extends AppenderSkeleton {
 			return "";
 		}
 
-		final StringBuilder builder = new StringBuilder(DATE_FORMAT.get().format(new Date(timeStamp)));
+		final StringBuilder builder = new StringBuilder(
+				LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneOffset.UTC).format(DATE_FORMAT));
 		// RFC 3164 says leading space, not leading zero on days 1-9
 		if (builder.charAt(4) == '0') {
 			builder.setCharAt(4, ' ');
@@ -650,7 +653,7 @@ public class SyslogAppender64k extends AppenderSkeleton {
 	 */
 	private void sendLayoutMessage(final String message) {
 		syslogQuietWriter.ifPresent(syslogWriter -> {
-			final String packet = createPacket(getPacketHeader(new Date().getTime()), message);
+			final String packet = createPacket(getPacketHeader(Instant.now().toEpochMilli()), message);
 			syslogWriter.setLevel(6);
 			syslogWriter.write(packet);
 		});
